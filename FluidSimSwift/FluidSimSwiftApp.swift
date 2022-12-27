b3b828840b8e2ecb9c7c062f0dfbbde8554668d9
yukiny0811 <yukiny0811@gmail.com>
2022-12-27 14:57:49 +0900

    Initial Commit

---
+++ FluidSimSwift/FluidSimSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluidSimSwiftApp.swift
+//  FluidSimSwift
+//
+//  Created by Yuki Kuwashima on 2022/12/27.
+//
+
+import SwiftUI
+
+@main
+struct FluidSimSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
